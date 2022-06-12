17027380e898a2d36a9e2ea6517302e1bd52897d
Tait Weinert <tait.weinert@outlook.com>
2022-06-12 09:46:31 +0930

    shuffle cards functioning

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Card(props) {
+
   return (
     <div>
       Display image and text based on ID: {props.cardNum}
--- src/components/CardHolder.js
+++ src/components/CardHolder.js
@@ -2,19 +2,43 @@ import React, { useState, useEffect } from "react";
 import Card from "./Card";
 
 function CardHolder(props) {
-  /*
-  useEffect to set random card placement each render
-  */
   const [cardOrder, setCardOrder] = useState([1, 2, 3, 4]);
+  const [firstCard, setFirstCard] = useState(cardOrder[0]);
 
   useEffect(() => {
-    
-  });
-  
+    const shuffleCardOrder = () => {
+      setCardOrder(shuffle(cardOrder));
+      setFirstCard(cardOrder[0]);
+      console.log(cardOrder);
+    };
+
+    const button = document.getElementById("shuffleBtn");
+    button.addEventListener("click", shuffleCardOrder);
+
+    return () => {
+      button.removeEventListener("click", shuffleCardOrder);
+    };
+  }, [cardOrder]);
+
+  function shuffle(array) {
+    let currentIndex = array.length, randomIndex;
+
+    while (currentIndex != 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+  }
+
   return (
     <div>
+      <button id="shuffleBtn">Shuffle</button>
       <Card cardNum={cardOrder[0]}/>
-      <Card cardNum={cardOrder[1]} />
+      <Card cardNum={cardOrder[1]}/>
       <Card cardNum={cardOrder[2]}/>
       <Card cardNum={cardOrder[3]}/>
     </div>
